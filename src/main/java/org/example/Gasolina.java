3323e28badd732f65bafb998259044830791de53
lizethM <morenomorenolizethvanesa@gmail.com>
2026-02-11 08:48:11 -0500

    primer cambio

--- src/main/java/org/example/Gasolina.java
+++ src/main/java/org/example/Gasolina.java
@@ -2,12 +2,15 @@ package org.example;
 
 public class Gasolina {
 
+    // Atributo privado tipo de gasolina
     private String tipo;
 
+    // Constructor que inicializa el tipo de gasolina
     public Gasolina(String tipo) {
         this.tipo = tipo;
     }
 
+    // Método getter para obtener el tipo de gasolina
     public String getTipo() {
         return tipo;
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,18 +3,29 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        // Crear un objeto carro usando el constructor vacío
         carro c1 = new carro();
-        c1.setMarca("Toyota");
-        c1.setModelo("Corolla");
-        c1.setColor("Rojo");
 
+        // Usar los métodos set para asignar los valores a los atributos del carro c1
+        c1.setMarca("Toyota");      // Marca = "Toyota"
+        c1.setModelo("Corolla");    // Modelo = "Corolla"
+        c1.setColor("Rojo");        // Color = "Rojo"
+
+        // Crear un segundo carro usando el constructor con parámetros
+        // Los atributos se asignan directamente en la creación del objeto
         carro c2 = new carro("Mazda", "CX-5", "Negro");
 
-        Gasolina gasolina = new Gasolina("Extra");
+        // Crear un objeto Gasolina con tipo "Extra"
+        Gasolina gasolina = new Gasolina("diesel");
 
+        // Hacer que el carro c1 conduzca usando la gasolina creada
+        // Esto llama al método conducir() de c1
         c1.conducir(gasolina);
 
+        // Imprimir los datos de c1 usando toString()
         System.out.println(c1.toString());
+
+        // Imprimir los datos de c2 usando toString()
         System.out.println(c2.toString());
     }
 }
